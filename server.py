e76dc160d4a45bff8a47f111c1549014a14934aa
gyupro <79894531+gyupro@users.noreply.github.com>
2023-01-19 13:28:38 +0900

    Update server.py
    Update code when a car is not detected

--- server.py
+++ server.py
@@ -44,7 +44,29 @@ def detect(car_m, lp_m, reader, path):
     result_text = []
 
     if len(locs) == 0:
-        result_text.append('검출된 차 없음')
+        
+        result = lp_m(im)
+        if len(result) == 0:
+            result_text.append('검출된 차 없음')
+        else:
+            
+            for rslt in result.xyxy[0]:
+                x2,y2,x3,y3 = [item1.cpu().detach().numpy().astype(np.int32) for item1 in rslt[:4]]
+                try:
+                    extra_boxes = 0
+                    im = cv2.cvtColor(cv2.resize(to_draw[y2 - extra_boxes:y3 + extra_boxes,x2 - extra_boxes:x3 + extra_boxes], (224,128)), cv2.COLOR_BGR2GRAY)
+                    text = reader.recognize(im)[0][1]
+                    result_text.append(text)
+                except Exception as e:
+                    return cv2.resize(to_draw, (1280,1280)), ""
+                img_pil = Image.fromarray(to_draw)
+                draw = ImageDraw.Draw(img_pil)
+                draw.text( (x2-100,y2-300),  text, font=font, fill=(255,0,255))
+                to_draw = np.array(img_pil)
+                st.write((x2.item(),y2))
+                cv2.rectangle(to_draw, (x2.item(),y2.item()),(x3.item(),y3.item()),(255,0,255),10)
+
+            return cv2.resize(to_draw, (1280,1280)), result_text
     
     for idx, item in enumerate(locs):
         x,y,x1,y1=[it.cpu().detach().numpy().astype(np.int32) for it in item[:4]]
